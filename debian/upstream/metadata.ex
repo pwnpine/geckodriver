8409ffe70f9413fd2a5e674aab7dbf0a46bac9af
joe <joe@rstrike.my.id>
2025-03-25 12:09:50 +0000

    Initial commit

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/geckgodriver/issues
+# Bug-Submit: https://github.com/<user>/geckgodriver/issues/new
+# Changelog: https://github.com/<user>/geckgodriver/blob/master/CHANGES
+# Documentation: https://github.com/<user>/geckgodriver/wiki
+# Repository-Browse: https://github.com/<user>/geckgodriver
+# Repository: https://github.com/<user>/geckgodriver.git
